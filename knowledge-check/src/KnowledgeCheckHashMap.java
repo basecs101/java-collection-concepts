9ac9be25a72f84258ba240982b4a8ff40ba5a621
Vikram Gupta <vikramguptavit@gmail.com>
2023-01-28 09:17:59 +0530

    Update KnowledgeCheckHashMap.java and Add concurrent modification exception.

--- knowledge-check/src/KnowledgeCheckHashMap.java
+++ knowledge-check/src/KnowledgeCheckHashMap.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
-class Test{
+import java.util.Map;
+class Test {
     String name;
     public Test(String name) {
         this.name = name;
@@ -9,11 +10,18 @@ class Test{
         return 1;
     }
 }
+
 class KnowledgeCheckHashMap {
     public static void main(String[] args) {
-        HashMap<Test, String> map = new HashMap<>();
-        map.put(new Test("a"),"a");
-        map.put(new Test("b"),"b");
-        System.out.println(map.get(new Test("b")) + " "+ map.get(new Test("b")));
+        Map<Test, String> map = new HashMap<>();
+        Test test1 = new Test("a");//test1=1000h
+        Test test2 = new Test("b");//test2=2000h
+        map.put(test1, "abc");
+        map.put(test2, "bcd");
+        Test test3 = new Test("a");//test3=3000h
+        Test test4 = new Test("b");////test4=4000h
+
+        System.out.println(map.get(test3) + " " + map.get(test4));
+
     }
 }
