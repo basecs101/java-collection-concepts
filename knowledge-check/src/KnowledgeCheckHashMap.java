a09ad9d80aec4af22adaea1672e0bae0ea26f9ff
Vikram Gupta <vikramguptavit@gmail.com>
2023-01-28 15:39:42 +0530

    Update KnowledgeCheckHashMap.java
    with and without hashCode

--- knowledge-check/src/KnowledgeCheckHashMap.java
+++ knowledge-check/src/KnowledgeCheckHashMap.java
@@ -5,6 +5,12 @@ class TestMap {
     public TestMap(String name) {
         this.name = name;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        return true;
+    }
+
     @Override
     public int hashCode() {
         return 1;
@@ -16,13 +22,22 @@ class KnowledgeCheckHashMap {
         Map<TestMap, String> hashMap = new HashMap<>();
         TestMap test1 = new TestMap("a");//test1=1000h
         TestMap test2 = new TestMap("b");//test2=2000h
-        hashMap.put(test1, "abc");
-        hashMap.put(test2, "bcd");
+        System.out.println(hashMap);
+        hashMap.put(test1, "a");
+        System.out.println(hashMap);
+
+        hashMap.put(test2, "b");
+        System.out.println(hashMap);
+
         TestMap test3 = new TestMap("a");//test3=3000h
+
         TestMap test4 = new TestMap("b");////test4=4000h
 
-        System.out.println(hashMap.get(test3) + " " + hashMap.get(test4));
+        System.out.println(hashMap.get(test3));
+        System.out.println(hashMap);
 
+        System.out.println(hashMap.get(test4));
+        System.out.println(hashMap);
     }
 }
 /*
@@ -30,7 +45,7 @@ class KnowledgeCheckHashMap {
 HashMap works in 2 passes:-
 
 1 -> Use HashCode and then find index of bucket using hash function
-2 -> Use equals method when two keys have same hashCode
+2 -> Use equals method when two keys have same hashCode// Key content is compared when same hashcode.
 
 There are 3 Scenarios->
 
@@ -50,6 +65,59 @@ Visualize it as below:-
 6[]
 7[]
 
-1. In case of hashMap.get(test3) --> no key present in the map -->returns null
+In case of equals method is not overridden and doesn't return true all the time.
+1. hashMap.get(test3) --> no key present in the map -->returns null
 2. same for hashMap.get(test4) --> no key present in the map -->returns null
+
+---------------------+++++++++++++++++++++++++------------------------
+
+In case of equals method is overridden and returns true all the time.
+1. hashMap.get(test3) --> no key present in the map -->returns null
+2. same for hashMap.get(test4) --> no key present in the map -->returns null
+
+put test1 -->
+Visualize it as below:-
+0[]
+1[] -->[1000h, "a"]-->null
+2[]
+3[]
+4[]
+5[]
+6[]
+7[]
+
+put test2 -->
+Visualize it as below:-
+0[]
+1[] -->[2000h, "b"]-->null
+2[]
+3[]
+4[]
+5[]
+6[]
+7[]
+
+now for hashMap.get(test3) --> hashCode(that is 1) is already present in the bucket and equals returns true.
+Hence, it will return "b"
+Visualize it as below:-
+0[]
+1[] -->[2000h, "b"]-->null
+2[]
+3[]
+4[]
+5[]
+6[]
+7[]
+
+now for hashMap.get(test4) --> hashCode(that is 1) is already present in the bucket and equals returns true.
+Hence, it will return "b"
+Visualize it as below:-
+0[]
+1[] -->[2000h, "b"]-->null
+2[]
+3[]
+4[]
+5[]
+6[]
+7[]
  */
